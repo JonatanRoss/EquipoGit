548e277d75cb4838408d03e8bf4831243dc94d07
JonatanRoss <jonnajpr@gmail.com>
2026-07-03 13:09:37 -0600

    agregue u

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("d");
         System.out.println("Jona gigachad");
+        System.out.println("ks");
     }
 }
