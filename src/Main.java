af109445678fa25eb785711a32aac8db70c6a654
CurryBoard1 <25420045@uttcampus.edu.mx>
2026-07-03 13:29:34 -0600

    chuy exitoso

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("d");
-        System.out.println("Jona gigachad");
+        System.out.println("Jona cuck");
         //hola
-        if(chuy == gay) {
+        if(chuy == exitosochad) {
             System.out.println("lo normal");
         }
     }
