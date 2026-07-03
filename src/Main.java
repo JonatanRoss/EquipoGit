cbf593d1fc4da07c4abea338d828ce5074f2f47a
CurryBoard1 <25420045@uttcampus.edu.mx>
2026-07-03 13:20:52 -0600

    jona es gei

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("d");
         System.out.println("Jona gigachad");
+        //hola
         if(chuy == gay) {
             System.out.println("lo normal");
         }
     }
 }
+//jona  = gay
