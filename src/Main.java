b0caeae878893ac6f6faf27a78e1ea75d2bd723f
JonatanRoss <jonnajpr@gmail.com>
2026-07-03 13:15:20 -0600

    agregue ujh

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("d");
         System.out.println("Jona gigachad");
+        // hola
         if(chuy == gay) {
             System.out.println("lo normal");
         }
